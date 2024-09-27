ac84e98d1a6f7db0deb76b6586d482c03330a420
ddPn08 <pyn.g.1125@gmail.com>
2024-09-27 21:06:31 +0900

    starshipの初期化処理を追加

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 # install nushell
-brew install nushell tmux nvim
+brew install nushell tmux nvim starship
+
+mkdir ~/.cache/starship
+starship init nu | save -f ~/.cache/starship/init.nu
 
 nu setup.nu
