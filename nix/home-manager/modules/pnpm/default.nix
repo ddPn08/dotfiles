d0e760d7995885d9dd34c46b506bcb412f92ce0f
ddpn08 <pyn.g.1125@gmail.com>
2025-02-25 10:49:42 +0900

    pnpm_homeを設定

---
+++ nix/home-manager/modules/pnpm/default.nix
@@ -0,0 +1,10 @@
+{
+  programs.fish = {
+    shellInit = ''
+      set -x PNPM_HOME="$HOME/.local/share/pnpm"
+      if not contains $PNPM_HOME $PATH
+        set -x PATH $PNPM_HOME $PATH
+      end
+    '';
+  };
+}
