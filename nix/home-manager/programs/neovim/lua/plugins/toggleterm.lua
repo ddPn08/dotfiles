cfb99875c6da2cf2442f7991aaf40c845d60f424
ddpn08 <pyn.g.1125@gmail.com>
2025-07-09 16:33:33 +0900

    toggletermの設定を更新

--- nix/home-manager/programs/neovim/lua/plugins/toggleterm.lua
+++ nix/home-manager/programs/neovim/lua/plugins/toggleterm.lua
@@ -9,6 +9,13 @@ return {
         shade_filetypes = {},
         direction = "float"
       })
+      
+      -- Terminal mode keymaps
+      vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
+      vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]], { desc = 'Move to left window' })
+      vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]], { desc = 'Move to bottom window' })
+      vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]], { desc = 'Move to top window' })
+      vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]], { desc = 'Move to right window' })
     end,
   },
 }
