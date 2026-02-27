0cc4ff791bcba285b0d197c63d34dc859fc87775
ddpn08 <pyn.g.1125@gmail.com>
2026-02-27 20:42:03 +0900

    tmuxの設定を変更

--- nix/home-manager/programs/tmux/default.nix
+++ nix/home-manager/programs/tmux/default.nix
@@ -28,10 +28,32 @@ in
     baseIndex = 0;
     prefix = "C-p";
     sensibleOnTop = false;
+    keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [ sensible ] ++ [ tmux-powerline ];
     extraConfig = ''
       set -g pane-active-border-style fg=cyan,bold
       set -g pane-border-style fg=colour240
+
+      # Vim-like pane navigation
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # Vim-like pane resizing
+      bind -r H resize-pane -L 5
+      bind -r J resize-pane -D 5
+      bind -r K resize-pane -U 5
+      bind -r L resize-pane -R 5
+
+      # Vim-like split
+      bind v split-window -h -c "#{pane_current_path}"
+      bind s split-window -v -c "#{pane_current_path}"
+
+      # Vim-like copy mode
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      bind -T copy-mode-vi C-v send-keys -X rectangle-toggle
     '';
   };
 }
